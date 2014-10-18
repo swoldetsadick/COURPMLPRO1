d6e06c1c937f16f72872b8a4eccfd05dde9f1fa5
swoldetsadick <swoldetsadick@ineteconomics.org>
2014-10-18 04:28:18 +0200

    messages

--- rdm.R
+++ rdm.R
@@ -5,21 +5,20 @@ data <- read.csv("./pml-training.csv", header = TRUE, sep = ",")
 library(caret)
 library(plyr)
 nsv <- nearZeroVar(data, saveMetrics = TRUE)
-nsv2 <- ifelse(nsv$nzv == TRUE, 1, 0)
-data1 <- data.frame(rbind(data, nsv2))
-newdata <- data1[, -which((data1[19623,]) == 1)]
-newdata1 <- newdata[, -which(is.na(newdata[19623,]))]
+data <- data.frame(rbind(data, ifelse(nsv$nzv == TRUE, 1, 0)))
+newdata <- data[, -which((data[nrow(data),]) == 1)]
+newdata <- newdata[, -which(is.na(newdata[nrow(data),]))]
 
-
-
-oui <- data.frame(cbind(rownames(nsv),nsv$nzv))
-names(oui) <- c("name", "zero")
-non <- subset(oui, zero == "FALSE")
+non <- subset(nsv, nzv == FALSE)
 non$ind <- rep(0, times = nrow(non))
-for (i in 1:nrow(non)){if((non$name[i] %in% colnames(newdata1) ) == TRUE) {non$ind[i] <- "yes"} else {non$ind[i] <- "no"}}
+for (i in 1:nrow(non)){if((row.names(non[i,]) %in% colnames(newdata) ) == TRUE)
+                                {non$ind[i] <- "yes"} else {non$ind[i] <- "no"}}
 a <- subset(non, ind == "no")
-me <- as.data.frame(matrix(rep(0, times = nrow(data1)*length(a)), ncol = length(a)))
-for(i in 1:length(a)){ me[,i] <- data1[,grep(a[i,1], colnames(data1))] }
-names(me) <- a$name
-wow <- as.data.frame(cbind(me, newdata1))
-
+oui <- as.data.frame(matrix(rep(0, times = nrow(data)*nrow(a)), ncol = nrow(a)))
+for(i in 1:nrow(a)){oui[,i] <- data[,grep(row.names(a[i,]), colnames(data))]}
+names(oui) <- row.names(a)
+data <- as.data.frame(cbind(oui, newdata))
+data <- data[-nrow(data),]
+#imputing missing values
+#pca
+prComp <- prcomp(data[,4:ncol(data)])
